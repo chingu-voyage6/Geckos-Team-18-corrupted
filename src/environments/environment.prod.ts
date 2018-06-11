5e9cabc05350474667daea08bc2c250601d1cab1
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-06-11 22:04:17 +0300

    Updated environment structure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // replace with your firebase config
+  firebase: {
+    apiKey: 'your-api-key',
+    authDomain: 'your-app.firebaseapp.com',
+    databaseURL: 'https://your-app.firebaseio.com',
+    projectId: 'your-app',
+    storageBucket: 'your-app.appspot.com',
+    messagingSenderId: 'your-message-sender-id'
+  }
 };

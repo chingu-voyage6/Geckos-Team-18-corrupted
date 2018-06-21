a3f62c96983518cbf75f901be0cc2f7e9cdebcfb
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-06-21 22:54:11 +0300

    Added missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AngularFireModule } from 'angularfire2';
+
 import { environment } from '../environments/environment';
-import { AppComponent } from './modules/core/components/app/app.component';
+
 import { CoreModule } from './modules/core/core.module';
+
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
   declarations: [AppComponent],

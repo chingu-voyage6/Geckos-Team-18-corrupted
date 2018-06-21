3be788c06266c0b40e7b6ffa570cd573d1739b88
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-06-21 21:55:34 +0300

    Fix: Added material module import

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './routing/core-routing/core-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { CoreComponent } from './components/core/core.component';
 
 @NgModule({
-  imports: [CommonModule, CoreRoutingModule],
+  imports: [CommonModule, CoreRoutingModule, MaterialModule],
   declarations: [CoreComponent],
   exports: [CoreComponent]
 })

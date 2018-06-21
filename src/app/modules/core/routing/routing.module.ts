26e2b028a4415b37a0b1aedf150e40ef6313126f
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-06-21 22:54:33 +0300

    Refactored core component

--- src/app/modules/core/routing/routing.module.ts
+++ src/app/modules/core/routing/routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from '../components/test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: TestComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class CoreRoutingModule {}
+export class RoutingModule {}

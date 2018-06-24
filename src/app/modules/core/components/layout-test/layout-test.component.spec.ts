3bd6366184ab772106facf1e34c7180c2194ce02
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-06-25 01:25:54 +0300

    Added test component

--- src/app/modules/core/components/layout-test/layout-test.component.spec.ts
+++ src/app/modules/core/components/layout-test/layout-test.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestComponent } from './test.component';
+import { LayoutTestComponent } from './layout-test.component';
 
-describe('TestComponent', () => {
-  let component: TestComponent;
-  let fixture: ComponentFixture<TestComponent>;
+describe('LayoutTestComponent', () => {
+  let component: LayoutTestComponent;
+  let fixture: ComponentFixture<LayoutTestComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestComponent ]
+      declarations: [ LayoutTestComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TestComponent);
+    fixture = TestBed.createComponent(LayoutTestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/modules/core/components/layout-test/layout-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-test',
+  templateUrl: './layout-test.component.html',
+  styleUrls: ['./layout-test.component.css']
+})
+export class LayoutTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

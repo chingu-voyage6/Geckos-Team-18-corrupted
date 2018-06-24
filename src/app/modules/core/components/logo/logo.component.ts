36644f8747cd798fb231b2358401473fb40d0560
Unknown <vero@nuagesdencre.com>
2018-06-24 13:54:12 +0100

    created logo component

---
+++ src/app/modules/core/components/logo/logo.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.css']
+})
+export class LogoComponent implements OnInit {
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'breezycards',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('/src/assets/logochingu_sm.svg')
+    );
+  }
+  ngOnInit() {
+  }
+
+}

4948581d7a335e3c35f93ebed28e3910783f8037
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-06-25 01:26:04 +0300

    Added auth guard

---
+++ src/app/modules/auth/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+import { tap, map, take } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.auth.user.pipe(
+      take(1),
+      map(user => !!(user && user.displayName)),
+      tap(authenticated => {
+        if (!authenticated) {
+          this.router.navigate(['/login']);
+        }
+      })
+    );
+  }
+}

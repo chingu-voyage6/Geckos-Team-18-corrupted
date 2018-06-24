c80d4bc524661d323da34c61b29c2967484db5c4
Unknown <vero@nuagesdencre.com>
2018-06-24 13:54:12 +0100

    created logo component

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MaterialModule } from '../material/material.module';
 
 import { TestComponent } from './components/test/test.component';
 import { CoreComponent } from './components/core/core.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, MaterialModule],
-  declarations: [CoreComponent, TestComponent, ToolbarComponent],
+  imports: [CommonModule, RouterModule, MaterialModule, HttpClientModule],
+  declarations: [CoreComponent, TestComponent, ToolbarComponent, LogoComponent],
   exports: [CoreComponent, TestComponent, ToolbarComponent]
 })
 export class CoreModule {}

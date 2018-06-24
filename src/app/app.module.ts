68ee9aab99bf2e3445887d082aa1010f87adfcf7
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-06-25 00:49:54 +0300

    Removed unnecessary dependencies
    Forms module and reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AngularFireModule } from 'angularfire2';
 

57a7b7a899adb511524a2bb6272a3448d3b2fa27
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-07-08 18:02:23 +0300

    Added sidenav component

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -7,10 +7,16 @@ import { RoutingModule } from './routing/routing.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule, RoutingModule],
-  declarations: [ToolbarComponent, LogoComponent, LandingComponent],
-  exports: [ToolbarComponent, LogoComponent, LandingComponent]
+  declarations: [
+    ToolbarComponent,
+    LogoComponent,
+    LandingComponent,
+    SidenavComponent
+  ],
+  exports: [ToolbarComponent, LogoComponent, LandingComponent, SidenavComponent]
 })
 export class CoreModule {}

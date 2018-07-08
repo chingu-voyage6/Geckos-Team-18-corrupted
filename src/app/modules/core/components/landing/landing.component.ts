ced9bf2827099eb2dd8690e22e5e47eaf836f508
Unknown <vero@nuagesdencre.com>
2018-07-08 16:02:58 +0100

    wip - landing page

--- src/app/modules/core/components/landing/landing.component.ts
+++ src/app/modules/core/components/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ViewChild } from '@angular/core';
+import { OnInit, Component, ViewChild } from '@angular/core';
 import { MediaChange, ObservableMedia } from '@angular/flex-layout';
 import { MatGridList } from '@angular/material';
 
@@ -7,22 +7,8 @@ import { MatGridList } from '@angular/material';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements AfterContentInit {
-  @ViewChild('grid') grid: MatGridList;
+export class LandingComponent implements OnInit {
+  constructor() {}
 
-  gridByBreakpoint = {
-    xl: 2,
-    lg: 2,
-    md: 2,
-    sm: 1,
-    xs: 1
-  };
-
-  constructor(private observableMedia: ObservableMedia) {}
-
-  ngAfterContentInit() {
-    this.observableMedia.asObservable().subscribe((change: MediaChange) => {
-      this.grid.cols = this.gridByBreakpoint[change.mqAlias];
-    });
-  }
+  ngOnInit() {}
 }

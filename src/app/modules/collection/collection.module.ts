f50fe7724a32cc35bffc65e954db0df9be5c205b
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-07-10 23:58:37 +0300

    Added routing module

--- src/app/modules/collection/collection.module.ts
+++ src/app/modules/collection/collection.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RoutingModule } from './routing/routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, RoutingModule],
   declarations: []
 })
-export class CollectionModule { }
+export class CollectionModule {}

16be6221033352278b8dff6a912e652526c9a3c8
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-07-11 00:27:56 +0300

    Created collection service

--- src/app/modules/collection/collection.module.ts
+++ src/app/modules/collection/collection.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoutingModule } from './routing/routing.module';
+
+import { CollectionService } from './services/collection.service';
+
 import { CollectionsComponent } from './components/collections/collections.component';
 
 @NgModule({
   imports: [CommonModule, RoutingModule],
   declarations: [CollectionsComponent],
-  exports: [CollectionsComponent]
+  exports: [CollectionsComponent],
+  providers: [CollectionService]
 })
 export class CollectionModule {}

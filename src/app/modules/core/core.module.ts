7a4356ed5e2d503849f431f11e67a4edadd8570f
Unknown <vero@nuagesdencre.com>
2018-07-10 14:07:02 +0100

    layout service - wip

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { RoutingModule } from './routing/routing.module';
 
+import { LayoutService } from './services/layout.service';
+
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { LandingComponent } from './components/landing/landing.component';
@@ -17,6 +19,7 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     LandingComponent,
     SidenavComponent
   ],
+  providers: [LayoutService],
   exports: [ToolbarComponent, LogoComponent, LandingComponent, SidenavComponent]
 })
 export class CoreModule {}

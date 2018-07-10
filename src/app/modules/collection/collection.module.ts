ca5175b2338300d262005c72fb5fe9a4366bea6e
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-07-11 00:43:02 +0300

    Added firestore

--- src/app/modules/collection/collection.module.ts
+++ src/app/modules/collection/collection.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { AngularFirestore } from 'angularfire2/firestore';
+
 import { RoutingModule } from './routing/routing.module';
 
 import { CollectionService } from './services/collection.service';
@@ -10,6 +13,6 @@ import { CollectionsComponent } from './components/collections/collections.compo
   imports: [CommonModule, RoutingModule],
   declarations: [CollectionsComponent],
   exports: [CollectionsComponent],
-  providers: [CollectionService]
+  providers: [AngularFirestore, CollectionService]
 })
 export class CollectionModule {}

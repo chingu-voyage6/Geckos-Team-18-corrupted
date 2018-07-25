a705eda23c28d1b43f0ff96ddd587826ab560fcf
Unknown <vero@nuagesdencre.com>
2018-07-25 21:09:39 +0100

    updated card and card-side components
    tested using firebase & anonymous login

--- src/app/modules/collection/components/card-side/card-side.component.ts
+++ src/app/modules/collection/components/card-side/card-side.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CardSide } from '../../models/card-side.model';
 
 @Component({
   selector: 'app-card-side',
@@ -6,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-side.component.css']
 })
 export class CardSideComponent implements OnInit {
+  @Input() cardSide: CardSide;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

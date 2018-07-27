1a196c30c65f39f80fca5a86d268e527397d1183
Unknown <portnyankoanton@gmail.com>
2018-07-27 20:43:21 +0300

    collection component

--- src/app/modules/collection/components/collections/collections.component.ts
+++ src/app/modules/collection/components/collections/collections.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CollectionService } from '../../services/collection.service';
-import { Observable } from 'rxjs';
-import { Collection } from '../../models/collection.model';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material';
-import { CollectionDetailComponent } from '../collection-detail/collection-detail.component';
-import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-collections',
@@ -13,48 +7,13 @@ import { AuthService } from '../../../auth/services/auth.service';
   styleUrls: ['./collections.component.css']
 })
 export class CollectionsComponent implements OnInit {
-  collections: Observable<Collection[]>;
-  userCollections: Observable<Collection[]>;
-  createForm: FormGroup;
-
+	
   constructor(
-    private collectionService: CollectionService,
-    private fb: FormBuilder,
-    public dialog: MatDialog
-  ) {}
+	private collectionService: CollectionService,
+	
+  	) { }
 
   ngOnInit() {
-    this.createForm = this.fb.group({
-      name: ['', [Validators.required]],
-      public: false
-    });
-    this.collections = this.collectionService.collections;
-    this.userCollections = this.collectionService.userCollections;
   }
 
-  get name() {
-    return this.createForm.get('name');
-  }
-
-  get public() {
-    return this.createForm.get('public');
-  }
-
-  create() {
-    this.collectionService.createCollection({
-      name: this.name.value,
-      public: this.public.value
-    });
-  }
-
-  collectionDetail(collection: Collection): void {
-    const dialogRef = this.dialog.open(CollectionDetailComponent, {
-      width: '250px',
-      data: collection
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
 }
--- src/app/modules/collection/routing/routing.module.ts
+++ src/app/modules/collection/routing/routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CollectionsComponent } from '../components/collections/collections.component';
+import { CreateCollectionComponent } from '../components/create-collection/create-collection.component';
+
 
 export const routes: Routes = [
   {
     path: '',
     component: CollectionsComponent
+  },
+  {
+  	path: 'create',
+  	component: CreateCollectionComponent
   }
+  
 ];
 
 @NgModule({

43c41a1de1b8734bb5f8046b160d1c4e402faf70
Unknown <vero@nuagesdencre.com>
2018-07-27 19:26:42 +0100

    styling of the legal pages

--- src/app/modules/legal/components/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/modules/legal/components/terms-and-conditions/terms-and-conditions.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-terms-and-conditions',
   templateUrl: './terms-and-conditions.component.html',
-  styleUrls: ['./terms-and-conditions.component.css']
+  styleUrls: ['../legal.module.scss']
 })
 export class TermsAndConditionsComponent implements OnInit {
 
--- src/app/modules/legal/legal.module.ts
+++ src/app/modules/legal/legal.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { DisclaimerComponent } from './components/disclaimer/disclaimer.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
-
 import { RoutingModule } from './routing/routing.module';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   imports: [
-    CommonModule, RoutingModule
+    CommonModule, RoutingModule, MaterialModule
   ],
   declarations: [ DisclaimerComponent, PrivacyPolicyComponent, TermsAndConditionsComponent]
 })

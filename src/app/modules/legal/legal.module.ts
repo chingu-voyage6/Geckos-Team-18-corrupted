4cdc53c4d0cd0af0f15a5cc3458a7c43c0d27def
Unknown <vero@nuagesdencre.com>
2018-07-27 18:46:21 +0100

    adding routes to legal module + link via landing page

--- src/app/modules/legal/legal.module.ts
+++ src/app/modules/legal/legal.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DisclaimerComponent } from './disclaimer/disclaimer.component';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
+import { DisclaimerComponent } from './components/disclaimer/disclaimer.component';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
+import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
+
+import { RoutingModule } from './routing/routing.module';
+
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RoutingModule
   ],
   declarations: [ DisclaimerComponent, PrivacyPolicyComponent, TermsAndConditionsComponent]
 })

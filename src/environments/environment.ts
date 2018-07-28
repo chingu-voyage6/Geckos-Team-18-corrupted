6f17f11834bab8e4531e475f43d380156dbb4e90
vy74u745 <vy74u745.p37r1k45@gmail.com>
2018-07-28 18:43:49 +0300

    Updated environment structure

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  // replace with your firebase config
+  firebase: {
+    apiKey: 'your-staging-api-key',
+    authDomain: 'your-staging-app.firebaseapp.com',
+    databaseURL: 'https://your-staging-app.firebaseio.com',
+    projectId: 'your-staging-app',
+    storageBucket: 'your-staging-app.appspot.com',
+    messagingSenderId: 'your-staging-message-sender-id'
+  }
+};
